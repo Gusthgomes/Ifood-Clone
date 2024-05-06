f482c8e730d947faa6f1374b84c395822c909234
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-05-06 16:56:31 -0300

    feat: add promo banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import CategoryList from "@/components/CategoryList";
 import Header from "@/components/Header";
 import Search from "@/components/Search";
+import Image from "next/image";
 
 
 export default function Home() {
@@ -14,7 +15,18 @@ export default function Home() {
       <div className="px-6 pt-6">
         <CategoryList/>
       </div>
-      
+
+      <div className="px-5 pt-6">
+        <Image 
+          src="/promo-banner-01.png" 
+          alt="Até 30% de desconto em todas as pizzas"
+          height={0}
+          width={0}
+          className="w-full h-auto object-contain"
+          sizes="100vw"
+          quality={100}
+        />
+      </div>
     </>
   );
 }
