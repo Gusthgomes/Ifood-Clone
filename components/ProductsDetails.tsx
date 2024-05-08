f1a96e3ce6771d15cb26b5c1357af3ead512ada2
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-05-08 17:07:39 -0300

    style: added border radius effect to product page

--- components/ProductsDetails.tsx
+++ components/ProductsDetails.tsx
@@ -34,7 +34,7 @@ const ProductsDetails = ({ products, complementaryProducts }: ProductsDetailsPro
     });
 
     return ( 
-        <div className="py-5">
+        <div className="relative z-50 mt-[-1.5rem] rounded-tl-3xl rounded-tr-3xl bg-white py-5">
 
                 {/* RESTAURANTE */}
             <div className="flex items-center gap-[0.375rem] px-5">
