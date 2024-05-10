bccc7d2a332695701e7076149560137fc47e6960
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-05-10 10:37:17 -0300

    feat: component sheet added in the product details page

--- components/ProductsDetails.tsx
+++ components/ProductsDetails.tsx
@@ -6,9 +6,12 @@ import { calculateProductsTotalPrice, formatCurrency } from "@/helpers/price";
 import { Prisma } from "@prisma/client";
 import { Button } from "./ui/button";
 import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import ProductList from "./ProductList";
 import DeliveryInfo from "./DeliveryInfo";
+import { CartContext } from "@/context/cart";
+import { Sheet, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet";
+import Cart from "./Cart";
 
 interface ProductsDetailsProps {
     products: Prisma.ProductsGetPayload<{
@@ -26,6 +29,14 @@ interface ProductsDetailsProps {
 
 const ProductsDetails = ({ products, complementaryProducts }: ProductsDetailsProps) => {
     const [quantity, setQuantity] = useState(1);
+    const [isCartOpen, setIsCartOpen] = useState(false);
+
+    const { addProductToCart } = useContext(CartContext);
+
+    const handleAddToCartClick = () => {
+        addProductToCart(products, quantity);
+        setIsCartOpen(true);
+    };
 
     const handleIncreaseQuantityClick =() => setQuantity(currentState => currentState + 1);
     const handleDecreaseQuantityClick =() => setQuantity(currentState => {
@@ -34,10 +45,11 @@ const ProductsDetails = ({ products, complementaryProducts }: ProductsDetailsPro
     });
 
     return ( 
-        <div className="relative z-50 mt-[-1.5rem] rounded-tl-3xl rounded-tr-3xl bg-white py-5">
+        <>
+            <div className="relative z-50 mt-[-1.5rem] rounded-tl-3xl rounded-tr-3xl bg-white py-5">
 
                 {/* RESTAURANTE */}
-            <div className="flex items-center gap-[0.375rem] px-5">
+                <div className="flex items-center gap-[0.375rem] px-5">
                 <div className="relative h-6 w-6">
                     <Image
                         src={products?.restaurant?.imageUrl}
@@ -47,13 +59,13 @@ const ProductsDetails = ({ products, complementaryProducts }: ProductsDetailsPro
                     />
                 </div>
                 <span className="text-xs text-muted-foreground">{ products?.restaurant?.name }</span>
-            </div>
+                </div>
 
                 {/* NOME DO PRODUTO */}
-            <h1 className="text-xl font-semibold mb-2 mt-1 px-5">{ products.name }</h1>
+                <h1 className="text-xl font-semibold mb-2 mt-1 px-5">{ products.name }</h1>
 
-            {/* PREÇO DO PRODUTO E QUANTIDADE */}
-            <div className="flex justify-between px-5">
+                {/* PREÇO DO PRODUTO E QUANTIDADE */}
+                <div className="flex justify-between px-5">
 
                 {/* PREÇO COM DESCONTO */}
                 <div>
@@ -91,27 +103,41 @@ const ProductsDetails = ({ products, complementaryProducts }: ProductsDetailsPro
                         <ChevronRightIcon/>
                     </Button>
                 </div>
+                </div>
+
+                {/* DADOS DA ENTREGA */}
+                <div className="px-5">
+                    <DeliveryInfo restaurant={ products.restaurant }/>
+                </div>
+                <div className="mt-6 space-y-3 px-5">
+                    <h3 className="font-semibold">Sobre</h3>
+                    <p className="text-sm text-muted-foreground">{ products.description }</p>
+                </div>  
+
+                <div className="mt-6 space-y-3">
+                    <h3 className="px-5 font-semibold">Sucos</h3>
+                    <ProductList product={complementaryProducts}/>
+                </div>
+
+                <div className="mt-6 px-5">
+                    <Button 
+                        onClick={ handleAddToCartClick }
+                        className="w-full font-semibold"
+                    >
+                        Adicionar a sacola
+                    </Button>
+                </div>     
             </div>
 
-            {/* DADOS DA ENTREGA */}
-            <div className="px-5">
-                <DeliveryInfo restaurant={ products.restaurant }/>
-            </div>
-            <div className="mt-6 space-y-3 px-5">
-                <h3 className="font-semibold">Sobre</h3>
-                <p className="text-sm text-muted-foreground">{ products.description }</p>
-            </div>  
-
-            <div className="mt-6 space-y-3">
-                <h3 className="px-5 font-semibold">Sucos</h3>
-                <ProductList product={complementaryProducts}/>
-            </div>
-
-            <div className="mt-6 px-5">
-                <Button className="w-full font-semibold">Adicionar a sacola</Button>
-            </div>     
-
-        </div>
+            <Sheet open={ isCartOpen } onOpenChange={ setIsCartOpen }>
+                <SheetContent>
+                    <SheetHeader>
+                        <SheetTitle className="text-left">Sacola</SheetTitle>
+                    </SheetHeader>
+                    <Cart/>
+                </SheetContent>
+            </Sheet>
+        </>
     );
 }
 export default ProductsDetails;
