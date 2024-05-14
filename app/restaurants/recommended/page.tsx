43f31287fee2fb891111b13d6f9e1639f21025c3
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-05-14 17:53:55 -0300

    feat: added property userFavoriteRestaurant on Recommended page

--- app/restaurants/recommended/page.tsx
+++ app/restaurants/recommended/page.tsx
@@ -1,7 +1,9 @@
 import Header from "@/components/Header";
 import RestaurantItem from "@/components/RestaurantItem";
+import { authOption } from "@/lib/auth";
 import { db } from "@/lib/prisma";
 import { Metadata } from "next";
+import { getServerSession } from "next-auth";
 
 export const metadata: Metadata = {
     title: "Restaurantes recomendados",
@@ -9,6 +11,16 @@ export const metadata: Metadata = {
 };
 
 const RecommendedPage = async () => {
+
+    const session = await getServerSession(authOption);
+    const userFavoriteRestaurant = await db.userFavoriteRestaurant.findMany({
+        where: {
+            userId: session?.user?.id,
+        },
+        include: {
+            restaurant: true,
+        },
+    });
 
     const restaurants = await db.restaurant.findMany({});
 
@@ -19,7 +31,7 @@ const RecommendedPage = async () => {
             <h2 className="text-lg font-semibold mb-6">Restaurantes recomendados</h2>
             <div className="flex w-full flex-col gap-6">
                 { restaurants.map( (restaurant) => (
-                    <RestaurantItem 
+                    <RestaurantItem userFavoritesRestaurants={userFavoriteRestaurant}
                         key={restaurant.id} 
                         restaurant={ restaurant }
                         className="min-w-full max-w-full"
