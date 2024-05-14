a5b7b021a916934ecef5037258b6146bbd5bca1f
Gusth Gomes <gusthgomesdarosa@gmail.com>
2024-05-14 18:07:56 -0300

    feat: added property userFavoriteRestaurant on restaurant page

--- app/restaurants/page.tsx
+++ app/restaurants/page.tsx
@@ -1,5 +1,8 @@
 import Restaurant from "@/components/Restaurant";
+import { authOption } from "@/lib/auth";
+import { db } from "@/lib/prisma";
 import { Metadata } from "next";
+import { getServerSession } from "next-auth";
 import { Suspense } from "react";
 
 export const metadata: Metadata = {
@@ -7,10 +10,20 @@ export const metadata: Metadata = {
     description: "Página de restaurantes",
 };
 
-const restaurantPage = () => {
+const restaurantPage = async () => {
+    const session = await getServerSession(authOption);
+    const userFavoriteRestaurant = await db.userFavoriteRestaurant.findMany({
+        where: {
+            userId: session?.user?.id,
+        },
+        include: {
+            restaurant: true,
+        },
+    });
+
     return (
     <Suspense>
-        <Restaurant/>
+        <Restaurant useFavoriteRestaurant={userFavoriteRestaurant}/>
     </Suspense>
     );
 }
